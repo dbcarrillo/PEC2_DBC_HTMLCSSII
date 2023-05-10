5bf78a8e39e7c86846a7a371ae8941718b648457
rai <rai@iMac-de-rai.local>
2023-05-10 21:46:25 +0200

    Primer Commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,11 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+import * as bootstrap from 'bootstrap';
+/**
+ * Write any other JavaScript below
+ */
